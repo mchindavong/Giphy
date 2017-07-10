39370d41507e71071fd1c4234c0f36a547b04752
Mollie <molliechindavong@gmail.com>
2017-07-09 23:38:15 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,44 @@
 $(document).ready(function() {
 var topics = [];
 
+function displayDisneyMovie() {
+
 var movie = $(this).data("search");
+  console.log(movie);
 
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
+  console.log(queryURL);
 
-	console.log(queryURL);
-
-	$.ajax({
+  $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-        	var results = response.data;
-        	console.log(results);
-        	for (var i = 0; i < results.length; i++) {
-        	
-        	var showDiv = $("<div class='col-md-4'>");
+          var results = response.data;
+          console.log(results);
+          for (var i = 0; i < results.length; i++) {
+          
+          var showDiv = $("<div class='col-md-4'>");
 
-        	var rating = results[i].rating;
-        	var defaultAnimatedSrc = results[i].images.fixed_height.url;
-        	var staticSrc = results[i].images.fixed_height_still.url;
-        	var showImage = $("<img>");
-        	var p = $("<p>").text("Rating: " + rating);
+          var rating = results[i].rating;
+          var defaultAnimatedSrc = results[i].images.fixed_height.url;
+          var staticSrc = results[i].images.fixed_height_still.url;
+          var showImage = $("<img>");
+          var p = $("<p>").text("Rating: " + rating);
 
-        	showImage.attr("src", staticSrc);
-        	showImage.addClass("disneyGiphy");
-        	showImage.attr("data-state", "still");
-        	showImage.attr("data-still", staticSrc);
-        	showImage.attr("data-animate", defaultAnimatedSrc);
-        	showDiv.append(p);
-        	showDiv.append(showImage);
-        	$("#gifArea").prepend(showDiv);
+          showImage.attr("src", staticSrc);
+          showImage.addClass("disneyGiphy");
+          showImage.attr("data-state", "still");
+          showImage.attr("data-still", staticSrc);
+          showImage.attr("data-animate", defaultAnimatedSrc);
+          showDiv.append(p);
+          showDiv.append(showImage);
+          $("#gifArea").prepend(showDiv);
 
         }
-	});
+  });
 }
 
-	$("#addMovie").on("click", function(event) {
+  $("#addMovie").on("click", function(event) {
         event.preventDefault();
         var newMovie = $("#disneyInput").val().trim();
         topics.push(newMovie);
@@ -46,7 +48,7 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6
       });
 
  
-	function displayButtons() {
+  function displayButtons() {
     $("#myButtons").empty();
     for (var i = 0; i < topics.length; i++) {
       var a = $('<button class="btn btn-primary">');
@@ -68,7 +70,7 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6
 
 
   function pausePlayGifs() {
-  	 var state = $(this).attr("data-state");
+     var state = $(this).attr("data-state");
       if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
